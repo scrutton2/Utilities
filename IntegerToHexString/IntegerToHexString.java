a1346ce5d54c04970f14bbb6728ce8ca6cbeb1c6
scrutton2 <43975989+scrutton2@users.noreply.github.com>
2019-10-05 12:47:34 -0400

    Add files via upload
    Integer to Hex String conversion utility.
    Does not use the standard Java method, as this is supposed to be an exercise in how to develop an algorithm to do this, although I have put this method in comments.
    Does use recursion.

---
+++ IntegerToHexString/IntegerToHexString.java
@@ -0,0 +1,13 @@
+
+
+//package com.mydeveloperplanet.jpmshello;
+
+package com.javacodegeeks.hello;
+
+public class IntegerToHexString {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World PKS");
+    }
+}
+
